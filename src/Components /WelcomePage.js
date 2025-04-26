05f86b2a69ab577e7c1e4e4a4ac0e5f19f43a9ea
victorgervac <victorgervacio33@gmail.com>
2025-04-25 21:32:44 -0600

    update welcome

--- src/Components /WelcomePage.js
+++ src/Components /WelcomePage.js
@@ -1,138 +1,76 @@
-import React, {useState} from "react"
-import  { ReactComponent as VictorMan1 } from '../assets/images/deskGuyLaptop.svg'
-import AboutComponent from "./AboutComponent"
-import Github from "./Github"
-import '../styles/WelcomePage.css'
-import '../styles/NavBarStyles.css'
-import NavBar from "./NavBar"
-import PastWork from "./PastWork"
-import { describeLine } from "../data"
-//import {Link} from "react-router-dom"
-// import FadeIn from "./FadeIn"
-// import { devices } from "../styles/mediaQuery"
-// import styled  from "styled-components";
+import React from "react";
+import { ReactComponent as VictorMan1 } from "../assets/images/deskGuyLaptop.svg";
+import AboutComponent from "./AboutComponent";
+import Github from "./Github";
+import NavBar from "./NavBar";
+import PastWork from "./PastWork";
+import { describeLine } from "../data";
 
-const FRONT_END = "A <strong>Software Developer</strong>"
+import "../styles/WelcomePage.css";
+import "../styles/NavBarStyles.css";
 
-function createMarkup(markup){
- return {__html: markup}
-}
+const FRONT_END = "A <strong>Software Developer</strong>";
 
-const WelcomePage = () =>{
- const [userName, setUserName] = useState('');
-//  const MyName(props){
-//    console.log("hello")
-//  }
+const createMarkup = (markup) => ({ __html: markup });
+
+const WelcomePage = () => {
   return (
-     <div className="container">
-        <div className="landing-content box">
-          <div className="component-name">
-            <div className="hello-line">
-               Hello, I'm 
-              <div className="name-div">
-                Victor
-              </div>
-            </div>
-            <div className="dev-line" dangerouslySetInnerHTML={createMarkup(FRONT_END)}>
-            </div>
-            <div className="describe-line" dangerouslySetInnerHTML={createMarkup(describeLine)}>
-            </div>
-          </div>
-          <div className="component-svg ">
-            <div id="svg-id" className="svg-component">
-              <VictorMan1/>
-            </div>
-          </div>
-          <div className="wrapper-scroll">
+    <div className="container">
+      {/* Landing Section */}
+      <section className="landing-content box">
+        {/* Text Content */}
+        <div className="component-name">
+          <h1 className="hello-line">
+            Hello, I'm <span className="name-div">Victor</span>
+          </h1>
 
-          <div className="arrow_container">
-            <a href="#past-work">
-              <div className="chevron"></div>
-              <div className="chevron"></div>
-              <div className="chevron"></div>
-              <span className="text">portfolio</span>
-            </a>
-          </div>
-          </div>
+          <h2
+            className="dev-line"
+            dangerouslySetInnerHTML={createMarkup(FRONT_END)}
+          />
 
+          <p
+            className="describe-line"
+            dangerouslySetInnerHTML={createMarkup(describeLine)}
+          />
         </div>
-        <div className="naviagtion-component">
-          <NavBar/>
+
+        {/* SVG Illustration */}
+        <div className="component-svg" id="svg-id">
+          <VictorMan1 />
         </div>
-        <div id="past-work" className="past-work-component box">
-          <PastWork/>
+
+        {/* Scroll Indicator */}
+        <div className="wrapper-scroll">
+          <a href="#past-work" className="arrow_container" aria-label="Scroll to portfolio section">
+            <div className="chevron" />
+            <div className="chevron" />
+            <div className="chevron" />
+            <span className="text">Portfolio</span>
+          </a>
         </div>
-        <div id="github" className="github-component box">
-          <Github/>
-        </div>
-        {/* <div id="contact-id" className="contact-component box">
-          <ContactComponent/>
-        </div> */}
-        <div id="about-id" className="about-component box">
-         <AboutComponent/>
-        </div>
+      </section>
+
+
+      {/* NavBar */}
+      <div className="navigation-component">
+        <NavBar />
       </div>
-  )
-}
-/*
-const styles={
-  container:{
-    display:"flex",
-    padding: "2rem",
-    height:"100vh",
-    
-  }, 
-   navBar:{
-    display:"flex",
-    padding:"1rem",
-    justifyContent:"space-around",
-   },
-header:{
-    position: "absolute",
-    top: "60%",
-    left: "30%",
-    transform: "translate(-50%, -50%)"
-  },
-  headerhH1:{
-    color:"white",
-    fontSize: "70px",
-    textTransform: "uppercase",
-    margin: "0",
-    mixBlendMode: "overlay",
-    display: "inline-block",
-   lineHeight: "1em"
-  },
-  headerLetter:{
-    display: "inline-block",
-    lineHeight: "1em"
-  },
-  sidebarText:{
-    color:"white",
-    fontSize: "70px",
-    textTransform: "uppercase",
-    margin: "0",s
-    
-    position: "absolute",
-    top: "5%",
-    right: "5%",
-    color: "transparent",
-    WebkitTextStroke: "2px rgb(165, 165, 165)"
-  },
-  portfolio:{
-  display: "flex row",
-  transform: "translate(-50%, -50%)",
-  textTransform: "uppercase",
-  textDecoration: "underline",
-  fontSize: "60px",
-  hover:"opacity: 0.3",
-  margin:"10rem"
-}
-};
-const fadeIn = keyframes`
-    from {top: 0px;}
-    to {top: 200px;}
-  `;
-  */
 
- 
+      {/* Other Sections */}
+      <section id="past-work" className="past-work-component box">
+        <PastWork />
+      </section>
+
+      <section id="github" className="github-component box">
+        <Github />
+      </section>
+
+      <section id="about-id" className="about-component box">
+        <AboutComponent />
+      </section>
+    </div>
+  );
+};
+
 export default WelcomePage;
