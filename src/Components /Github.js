9fae039ca63d69d7dde31c88969a7a0b19041177
victorgervac <victorgervacio33@gmail.com>
2025-08-07 15:15:49 -0600

    yarn calendar

--- src/Components /Github.js
+++ src/Components /Github.js
@@ -5,35 +5,57 @@ import { COLORS } from "../styles/styles";
 import "../styles/Github.css";
 import GithubCard from "./GithubCard";
 import { githubDescription } from "../data";
+import { useEffect, useRef } from "react";
+import "github-calendar/dist/github-calendar-responsive.css";
+
 
 function createMarkup(markup) {
   return { __html: markup };
+
 }
 
 const Github = () => {
+  
+  const calendarRef = useRef(null); 
+  useEffect(() => {
+    if (calendarRef.current && typeof GitHubCalendar === "function") {
+      GitHubCalendar(calendarRef.current, "victorgervac", {
+        responsive: true,
+        proxy: (victorgervac) => {
+          return fetch(`https://your-proxy.com/github?user=${victorgervac}`);
+        }
+      });
+    }
+  }, []);
+
   return (
     <div>
-      <a
-        className="component-header github"
-        href="https://github.com/victorgervac"
-        target="_blank"
-        rel="noreferrer"
-      >
         <Icon name="github" /> Github
-      </a>
       <div className="grid-container">
         <div className="paragraph-cards">
           <div
             className="github-paragraph"
             dangerouslySetInnerHTML={createMarkup(githubDescription)}
-          />
+            />
           <GithubCard />
         </div>
-        <img 
+        <div className="calendar" ref={calendarRef}>
+          {/* Loading placeholder */}
+          Loading the data just for you.
+        </div>
+          <a
+            className="component-header github"
+            href="https://github.com/victorgervac"
+            target="_blank"
+            rel="noreferrer"
+            >
+            </a>
+  
+        {/* <img 
           src="https://ghchart.rshah.org/victorgervac" 
           alt="GitHub Contributions Chart" 
           style={{ width: "90%", height: "auto", marginTop: "2rem" ,marginLeft: "2rem",marginBottom: "2rem"  }}
-        />
+        /> */}
       </div>
     </div>
   );
